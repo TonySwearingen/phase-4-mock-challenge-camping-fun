4e9063abcd8712810ff2480a7c56211cc1e9160f
unlimited1974@aol.com <unlimited1974@aol.com>
2022-04-11 13:31:39 -0500

    commit tree 7aa880ee17a4d966e005ac5837885642beb84896

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,14 @@
+class SignupsController < ApplicationController
+    def create
+        signup = Signup.create!(signup_params)
+        render json: signup.activity, status: :created
+    rescue ActiveRecord::RecordInvalid => e
+        render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    private
+
+    def signup_params
+        params.permit(:time, :activity_id, :camper_id)
+    end
+end
